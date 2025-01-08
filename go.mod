17262a7dbffe338ceb8291713aa20206958b7881
helmigandi <sugandihelmi@gmail.com>
2025-01-08 08:35:58 +0700

    Initial commit

---
+++ go.mod
@@ -0,0 +1,3 @@
+module github.com/helmigandi/go-concurrency-1
+
+go 1.23.4
---
+++ main.go
@@ -0,0 +1,42 @@
+package main
+
+import (
+	"fmt"
+	"math/rand"
+	"time"
+)
+
+type Order struct {
+	ID     int
+	status string
+}
+
+func main() {
+	orders := generateOrders(20)
+
+	processOrders(orders)
+
+	fmt.Println("All operations completed. Exiting...")
+}
+
+// Imagine this to send HTTP Request or some request to another microservice
+// where it actually processes the orders
+func processOrders(orders []*Order) {
+	for _, order := range orders {
+		// Generate random int between 0 and 500
+		time.Sleep(time.Duration(rand.Intn(500)) * time.Millisecond)
+		fmt.Printf("Processing order %d\n", order.ID)
+	}
+}
+
+// return pointer of orders because we want to manipulate or modify the orders
+func generateOrders(count int) []*Order {
+	orders := make([]*Order, count)
+	for i := 0; i < count; i++ {
+		orders[i] = &Order{
+			ID:     i,
+			status: "pending",
+		}
+	}
+	return orders
+}
